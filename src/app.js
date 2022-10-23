9da13cfe8c630c230cbd7d41d15fe0982c2e3132
JLL-5199 <113879978+JLL-5199@users.noreply.github.com>
2022-10-23 21:10:45 +1100

    Unit conversion
    HTML- added class and a href around the unit text
    
    Css - removed href attributes
    
    JS - added conversion for units and css functions

--- src/app.js
+++ src/app.js
@@ -25,15 +25,46 @@ function showTemperature(response) {
     let displayTemperature = document.querySelector("#degrees");
     let displayWind = document.querySelector("#wind");
 
+    celsiusTemperature = Math.round(response.data.temperature.current);
+
     displayCity.innerHTML = response.data.city;
     displayDate.innerHTML = showDate(response.data.time * 1000);
     displayDescription.innerHTML = response.data.condition.description;
     displayHumidity.innerHTML = Math.round(response.data.temperature.humidity);
     displayIcon.setAttribute("src", `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`);
     displayIcon.setAttribute("alt", response.data.condition.description);
-    displayTemperature.innerHTML = Math.round(response.data.temperature.current);
+    displayTemperature.innerHTML = Math.round(celsiusTemperature);
     displayWind.innerHTML = Math.round(response.data.wind.speed);
 }
+
+// Unit conversion Fahrenheit
+function showFahrenheitData(event) {
+    event.preventDefault();
+    celsiusLink.classList.remove("active");
+    fahrenheitLink.classList.add("active");
+    let fahrenheitTemperature = (celsiusTemperature * 1.8) + 32;
+    let displayTemperature = document.querySelector("#degrees");
+    displayTemperature.innerHTML = Math.round(fahrenheitTemperature);
+    
+}
+
+//Unit conversion Celsius
+function showCelsiusData(event) {
+    event.preventDefault();
+    fahrenheitLink.classList.remove("active");
+    celsiusLink.classList.add("active");
+    let displayTemperature = document.querySelector("#degrees");
+    displayTemperature.innerHTML = Math.round(celsiusTemperature);
+    
+}
+
+let celsiusTemperature = null;
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", showFahrenheitData);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", showCelsiusData);
 
 // Search Engine functionality
 function search(city) {
